4db3d030085749d912ed9f584bb68782da6b2154
manishk1001 <61675371+manishk1001@users.noreply.github.com>
2023-01-27 19:37:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,25 @@ terraform {
     }
   }
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = ami-0597375488017747e
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
